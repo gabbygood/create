7ec2ba679f20348730e59ee99b928f4fb2c2a472
gabbygood <85273191+gabbygood@users.noreply.github.com>
2021-09-24 01:48:04 +0000

    plus

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int aPlus = 5;
+    int bPlus = 5;
+    int cPlus = aPlus + bPlus;
+    System.out.println(cPlus);
+    int aMinus = 5;
+    int bMinus = 5;
+    int cMinus = aMinus - bMinus;
+    System.out.println(cMinus);
+    int aTimes = 5;
+    int bTimes = 5;
+    int cTimes = aTimes * bTimes;
+    System.out.println(cTimes);
+    int aDivide = 10;
+    int bDivide = 5;
+    int cDivide = aDivide / bDivide;
+    System.out.println(cDivide);
+    System.out.println("Hello Gabrielle");
   }
 }
